d2b44097e463d04165f91666994951781a7cf6b7
konraifen88 <konraifen88@googlemail.com>
2018-08-21 06:58:06 +0200

    Add the cloud configuration and a start/remove script for cf apps

--- src/main/java/de/konraifen88/home/docs/DocsApplication.java
+++ src/main/java/de/konraifen88/home/docs/DocsApplication.java
@@ -18,12 +18,14 @@ package de.konraifen88.home.docs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableZuulProxy
 @EnableSwagger2
 @SpringBootApplication
+@EnableEurekaClient
 public class DocsApplication {
 
     public static void main(String[] args) {
